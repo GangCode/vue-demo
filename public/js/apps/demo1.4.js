8252b1122c8c8c77747c2385b0b8b744a392e293
yinzhigang <yinzhigang@yundun.com>
2016-09-18 16:06:36 +0800

    删除文件

--- public/js/apps/demo1.4.js
+++ public/js/apps/demo1.4.js
@@ -1,6 +1,6 @@
-//1.定义子组件构造器
 var Child = Vue.extend({
     template: '#child_temp',
+    props: ['proEdit', 'proDelete'],//1.在子组件中定义props属性
     data: function () {
         return {
             items: [
@@ -12,18 +12,24 @@ var Child = Vue.extend({
     }
 });
 
-//2.定义父组件构造器，父组件内注册子组件
 var Parent = Vue.extend({
     template: '#parent_temp',
     components: {
         'child': Child
+    },
+    data: function () {//父组件中的数据
+        return  {
+            edit: '修改',
+            delete: '删除'
+        };
     }
 });
 
-//3.全局注册父组件
 Vue.component('parent', Parent);
 
-//4.创建组件实例
 new Vue({
     el: '#parent'
 });
+
+
+
