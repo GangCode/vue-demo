02469cb20408b02d25b49f824ca06d675cf94fc6
yinzhigang <yinzhigang@yundun.com>
2016-09-19 11:30:53 +0800

    删除文件

---
+++ public/js/apps/demo0.1.js
@@ -0,0 +1,114 @@
+/**
+ * v-bind:class
+ */
+new Vue({
+    el: '#test_class',
+    data: {
+        show: true
+    },
+    components: {
+        'bind-class': {
+            template: '#bind_class',
+            props: ['show']
+        }
+    }
+});
+
+/**
+ * 使用v-bind:style时style属性名称采用驼峰命名法
+ */
+new Vue({
+    el: '#test_style',
+    components: {
+        'bind-style': {
+            template: '#bind_style',
+            data: function () {
+                return{
+                    bindStyle: {
+                        color: 'red',
+                        fontSize: '20px',
+                        fontWeight: '600'
+                    }
+                };
+            }
+        }
+    }
+});
+
+/**
+ * v-on
+ */
+new Vue({
+    el: '#test_click',
+    data: {
+        show: false
+    },
+    components: {
+        'on-click': {
+            template: '#on_click',
+            props: ['show'],
+            methods: {
+                changeColor: function () {
+                    this.show = !this.show;
+                }
+            }
+        }
+    }
+});
+
+/**
+ * v-for
+ */
+new Vue({
+    el: '#test_for',
+    components:{
+        'v-for':{
+            template:'#for',
+            data:function(){
+                return {
+                    items:['item1','item2','item3']
+                };
+            }
+        }
+    }
+});
+
+/**
+ * v-if v-else
+ */
+new Vue({
+    el: '#test_if',
+    data: {
+        showPar: true
+    },
+    components: {
+        'if-else': {
+            template: '#if-else',
+            data:function(){
+                return {
+                    showChi:true
+                };
+            }
+        }
+    }
+});
+
+/**
+ * v-show v-else
+ */
+new Vue({
+    el: '#test_show',
+    data: {
+        showPar: true
+    },
+    components: {
+        'show-else': {
+            template: '#show-else',
+            data:function(){
+                return {
+                    showChi:false
+                };
+            }
+        }
+    }
+});
