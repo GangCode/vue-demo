0335d194487278863a6d81fc26c8eea9d00f0329
yinzhigang <yinzhigang@yundun.com>
2016-09-19 17:56:11 +0800

    删除文件

---
+++ public/js/apps/demo1.7.js
@@ -0,0 +1,53 @@
+var prodsData = [
+    {
+        '商品名称': 'mac air',
+        '商品单价': 10000
+    },
+    {
+        '商品名称': 'ipad',
+        '商品单价': 10000
+    },
+    {
+        '商品名称': 'iphone8',
+        '商品单价': 10000
+    },
+    {
+        '商品名称': 'apple watch',
+        '商品单价': 10000
+    }
+];
+
+var category = [
+    {name: '商品名称'},
+    {name: '商品单价'}
+];
+
+var selectList = ['商品名称', '商品单价'];
+
+var table = Vue.extend({
+    template: '#simp_tab',
+    props: ['cates', 'prods', 'query'],
+    methods: {
+        deleteItem: function (index) {
+            var tds = $('[data-id=' + index + ']').children('td');
+            var prodName = tds[0];
+            var prodPrice = tds[1];
+
+            //服务端删除数据成功后删除UI
+            this.prods.splice(index, 1);
+        }
+    }
+});
+
+Vue.component('simmple-tab', table);
+
+var v_tab = new Vue({
+    el: '#test_tab',
+    data: {
+        categorys: category,
+        products: prodsData,
+        searchQuery: '',
+        selectLists: selectList
+    }
+});
+
