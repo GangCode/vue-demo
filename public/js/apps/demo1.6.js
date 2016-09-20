84bc038817cd53220cdab25335d07b705d8c2b17
yinzhigang <yinzhigang@yundun.com>
2016-09-20 14:03:28 +0800

    删除文件

--- public/js/apps/demo1.6.js
+++ public/js/apps/demo1.6.js
@@ -1,7 +1,19 @@
-Vue.component('modal',{
-      template:'#modal-temp'
+Vue.component('modal', {
+    template: '#modal-temp',
+    props: ['show']
 });
 
 new Vue({
-    el:'#modal'
+    el: '#modal',
+    data: {
+        show: false
+    },
+    methods: {
+        showModal: function () {
+            this.show = true;
+        },
+        hiddenModal: function () {
+            this.show = false;
+        }
+    }
 });
